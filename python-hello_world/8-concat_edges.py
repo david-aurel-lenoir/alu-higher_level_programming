21820083384a64207d5fd6b863a276054733e6c4
david-aurel-lenoir <d.butarebit@alustudent.com>
2026-06-24 06:13:51 -0700

    6and 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, object-oriented, high-level programming language with dynamic semantics."
-print(f"{str[39:46]}{str[31:38]} {str[68:80]} {str[49:53]} {str[24:30]}")
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
