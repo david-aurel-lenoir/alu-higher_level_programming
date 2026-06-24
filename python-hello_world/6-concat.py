addb4c35dd11a1e0168eacea29158bf4da086f1f
david-aurel-lenoir <d.butarebit@alustudent.com>
2026-06-24 04:26:58 -0700

    2-9

---
+++ python-hello_world/6-concat.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str1 = "Welcome to "
+str2 = "Holberton school!"
+print(str1 + str2)
---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, object-oriented, high-level programming language with dynamic semantics."
+print(f"{str[39:46]}{str[31:38]} {str[68:80]} {str[49:53]} {str[24:30]}")
